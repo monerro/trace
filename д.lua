e92e329e4b93fd9e346cb5524c19a38a02e44575
monerro <xnamelesswastaken@gmail.com>
2026-01-13 21:51:46 +1100

    Update д.lua
    Add a function to load scripts from a remote URL

--- д.lua
+++ д.lua
@@ -1,1 +1,13 @@
+local BASE_URL = "https://raw.githubusercontent.com/monerro/trace/main/"
+
+local function Load(path)
+    local ok, res = pcall(function()
+        return loadstring(game:HttpGet(BASE_URL .. path))()
+    end)
+    if not ok then
+        warn("FAILED TO LOAD:", path)
+        warn(res)
+    end
+    return res
+end
 
